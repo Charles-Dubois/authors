a1ea342efd3cc28cd92b050eb695d9097fda0d4a
Charles Dubois <charles.dubois.h@gmail.com>
2022-03-07 14:06:04 +0100

    step 1.2

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const app = express();
-
+const authors = [
+  {
+    name: "Lawrence Nowell",
+    nationality: "UK",
+    books: ["Beowulf"],
+  },
+  {
+    name: "William Shakespeare",
+    nationality: "UK",
+    books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"],
+  },
+  {
+    name: "Charles Dickens",
+    nationality: "US",
+    books: ["Oliver Twist", "A Christmas Carol"],
+  },
+  {
+    name: "Oscar Wilde",
+    nationality: "UK",
+    books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"],
+  },
+];
 app.get("/", (_req, res, _next) => {
-  res.send("Hello from author");
+  res.json("Authors API");
+});
+
+app.get("/authors/:authorsId", (req, res, _next) => {
+  const authorsId = authors[parseInt(req.params.authorsId) - 1];
+  if (!authorsId) {
+    return res.json({ errorMessage: "The Id of our API start form 1 to 4" });
+  }
+  const authorInfos = `${authorsId.name}, ${authorsId.nationality}`;
+  res.json(authorInfos);
 });
 
 app.listen(8000, () => {
